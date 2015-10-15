1acecb9266777f4164ae5d6bda7b743a24619249
skoslitz <sebastian.koslitz@gmail.com>
2015-10-15 21:20:31 +0200

    add september 2015

---
+++ content/Drittes-Lebensjahr/september-2015.md
@@ -0,0 +1,52 @@
++++
+date = "2015-09-30"
+draft = true
+title = "September 2015"
+categories = "Drittes Lebensjahr"
+
++++
+
+### Ferienzeit an der Ostsee 
+
+{{% img src="/img/theo_09_2015_01.JPG" %}}
+
+{{% img src="/img/theo_09_2015_02.JPG" %}}
+
+{{% img src="/img/theo_09_2015_03.JPG" %}}
+
+### Theo beim täglichen Hühner füttern
+
+{{% img src="/img/theo_09_2015_04.JPG" %}}
+{{% img src="/img/theo_09_2015_05.JPG" %}}
+
+### Ausflug im Wald
+{{% img src="/img/theo_09_2015_06.JPG" %}}
+
+### Ich fahre mit'm Lastwagen
+{{% img src="/img/theo_09_2015_07.JPG" %}}
+
+### steife Brise
+{{% img src="/img/theo_09_2015_08.JPG" %}}
+{{% img src="/img/theo_09_2015_09.JPG" %}}
+{{% img src="/img/theo_09_2015_10.JPG" %}}
+
+### Eis mit Sahne. Hmm, lecker!
+{{% img src="/img/theo_09_2015_11.JPG" %}}
+
+### Huppala
+{{% img src="/img/theo_09_2015_12.JPG" %}}
+
+### So geht das!
+{{% img src="/img/theo_09_2015_13.JPG" %}}
+
+### Ich fahre mit'm Kanu.
+{{% img src="/img/theo_09_2015_14.JPG" %}}
+
+### Die Schafe kann man nicht streicheln!
+{{% img src="/img/theo_09_2015_15.JPG" %}}
+
+### Es gibt Eis!
+{{% img src="/img/theo_09_2015_16.JPG" %}}
+
+### Da geht es zum Schiff.
+{{% img src="/img/theo_09_2015_17.JPG" %}}
---
+++ theo_images.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Batch thumbnail generation script using PIL
+# For PIL jpeg options see @ http://www.effbot.org/imagingbook/format-jpeg.htm
+
+#!/usr/bin/env python
+import os.path
+import ImageFile
+import Image
+
+
+# The top argument for walk.  
+topdir = '.'
+ 
+# The arg argument for walk, and subsequently ext for step
+exten =  ('.jpg', '.JPG')
+ 
+def step(ext, dirname, names):
+
+    for name in names:
+
+        # check if file extension equals defined ones in var exten
+        if name.endswith(ext):
+            filepath = os.path.join(dirname, name)
+            try:
+                image = Image.open(filepath)
+                basewidth = 640
+
+                if image.size[0] < basewidth:
+                    basewidth = image.size[0]
+
+            except IOError, e:
+                # Report error, and then skip to the next argument
+                print "Problem opening", filepath, ":", e
+                continue
+
+            width_ratio = (basewidth/float(image.size[0]))
+            height = int((float(image.size[1])*float(width_ratio)))
+       
+            absolute_filepath = os.path.basename(filepath)
+            # Split the original filename into name and extension
+            (filename, extension) = os.path.splitext(absolute_filepath)
+
+            #new_name = filename[:9] + file_suffix + filename[9:]
+           
+            # Resize the image with maintained aspect ration 
+            # and ANTIALIAS downsampling filter
+            image = image.resize((basewidth,height), Image.ANTIALIAS)
+
+            print "Image " + filename + " ... is in processing"
+
+            # set buffer properly for saving image
+            ImageFile.MAXBLOCK = 2 * image.size[0] * image.size[1]
+            # Save image with renamed filepath, set jpeg options 
+            new_filepath = os.path.join(dirname, filename) 
+            image.save(new_filepath + '.jpg', progressive=True, quality=90)
+
+            print (new_filepath + ".jpg was saved")
+            
+ 
+# Start the walk through folders
+os.path.walk(topdir, step, exten)
